c2fbcd4fa019f1c8efa80c808c8b4c07212abcc7
Daniel Thorne <thorne.danielj@gmail.com>
2022-12-28 01:12:44 -0600

    Async testing.

--- qRPC.Test/Program.cs
+++ qRPC.Test/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace qRPC.Test
 {
@@ -13,41 +14,55 @@ namespace qRPC.Test
             Console.ReadKey();
             var doStuff = new DoStuff();
 
-            var server = new qRPC.Server.QrpcServer<IDoStuff>(doStuff, "127.0.0.1", 5000, Encoding.UTF8);
+            var server = new qRPC.Server.QrpcServer<IDoStuff>(doStuff, 5713, Encoding.UTF8);
 
-            var client = new qRPC.Client.QrpcClient<IDoStuff>(5000, "127.0.0.1", Encoding.UTF8);
-
+            var client = new qRPC.Client.QrpcClient<IDoStuff>(5713, "127.0.0.1", Encoding.UTF8);
+            var client2 = new qRPC.Client.QrpcClient<IDoStuff>(5713, "127.0.0.1", Encoding.UTF8);
+            
             CancellationToken token = new CancellationToken(false);
 
             //var task = server.Listen(token);
 
             try
             {
-                var res = client.Remote.HiWorld("Dan");
-                Console.WriteLine(res);
+                DateTime start = DateTime.UtcNow;
 
-                var b = client.Remote.HiWorld("Felix");
-                Console.WriteLine(b);
+                var now1 = Task.Run(() => client.Remote.GetNow());
 
-                var c = client.Remote.HiWorld("Dan");
-                Console.WriteLine(c);
+                var res = Task.Run(() => client.Remote.HiWorld("Dan"));
 
-                var d = client.Remote.HiWorld("Felix");
-                Console.WriteLine(d);
+                var b = Task.Run(() => client2.Remote.HiWorld("Felix"));
 
-                var x = client.Remote.TryAGuid(Guid.NewGuid());
-                Console.WriteLine(x);
+                var c = Task.Run(() => client.Remote.HiWorld("Papá"));
+
+                var d = Task.Run(() => client.Remote.HiWorld("Son"));
+
+                var x = Task.Run(() => client.Remote.TryAGuid(Guid.NewGuid()));
+
+                Console.WriteLine(res.Result);
+                Console.WriteLine(b.Result);
+                Console.WriteLine(c.Result);
+
+                Console.WriteLine(d.Result);
+
+                Console.WriteLine(x.Result);
+
+                Console.WriteLine($"{(DateTime.UtcNow - start).TotalSeconds}s");
 
                 Console.WriteLine(client.Remote.GetAGuid());
+                Console.WriteLine($"{(DateTime.UtcNow - start).TotalSeconds}s");
 
                 var z = client.Remote.Addition(4.3, 6.2);
                 Console.WriteLine($"4.3 + 6.2 = ${z}");
+                Console.WriteLine($"{(DateTime.UtcNow - start).TotalSeconds}s");
 
                 var ticks = client.Remote.TryADatetime(DateTime.UtcNow);
                 Console.WriteLine($"Number of ticks: {ticks}");
+                Console.WriteLine($"{(DateTime.UtcNow - start).TotalSeconds}s");
 
                 var e = client.Remote.WhatIsMyNumber("Gem", 478);
                 Console.WriteLine(e);
+                Console.WriteLine($"{(DateTime.UtcNow - start).TotalSeconds}s");
 
 
                 client.Remote.NotImplemented();
